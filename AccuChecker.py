f68b229eb74223e4036d2b3041591f26344cd759
jvalegre <juanvi89@hotmail.com>
2020-01-15 13:56:49 -0700

    Checks for prediction accuracy (just as a reminder for later)

---
+++ AccuChecker.py
@@ -0,0 +1,7 @@
+"""
+In this file, the program will split your data
+into train and test, and then it'll determine
+how good your model is as a predictor based on the
+4 rules that we specified in Jeff's article
+(R2 and Q2 of the test, k and R2 diff)
+"""
