79baa084ea71af94e999bc37f25f52bf46ee75b4
jvalegre <juanvi89@hotmail.com>
2020-01-23 12:11:24 -0700

    PCA reminder

---
+++ PCA_reminder.py
@@ -0,0 +1,6 @@
+'''
+add an option that if you specify some parameters you
+can make a PCA plot with its corresponding scree plot, and then
+the program recognizes the elbow and takes that
+number of PCs to feed the model
+'''
