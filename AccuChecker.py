5cf2a1e528de0155c842285c7aa3edacf825e46a
jvalegre <juanvi89@hotmail.com>
2020-01-23 16:50:49 -0700

    stats comments for checks

--- AccuChecker.py
+++ AccuChecker.py
@@ -4,4 +4,8 @@ into train and test, and then it'll determine
 how good your model is as a predictor based on the
 4 rules that we specified in Jeff's article
 (R2 and Q2 of the test, k and R2 diff)
+
+
+Also, add apply the "X random" method and tje Y random
+method (they should be either in Mihais or Bens code)
 """
