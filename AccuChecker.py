6512e3571a6f1937f1fb8f57375edb5f46496600
jvalegre <juanvi89@hotmail.com>
2020-01-23 16:53:12 -0700

    new stats comments for checks and PCA

--- AccuChecker.py
+++ AccuChecker.py
@@ -8,4 +8,11 @@ how good your model is as a predictor based on the
 
 Also, add apply the "X random" method and tje Y random
 method (they should be either in Mihais or Bens code)
+
+Add options about PCAs using chunks of variables: the PCA
+should be able to find the elbow in the scree plot, and then
+grab that number of PCS to reduce the number of variables
+(i.e. 20 variables correlated regarding sterics -- converted
+to --  4 PCs used by the model, and the same for electronics,
+and so on)
 """
