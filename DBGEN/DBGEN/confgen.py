63f18e733638766c5ebaec31bd021f56a3961f9b
shreesowndarya <sowndary64@gmail.com>
2020-04-14 18:44:33 -0600

    Coorected smoothing!

--- DBGEN/DBGEN/confgen.py
+++ DBGEN/DBGEN/confgen.py
@@ -146,7 +146,7 @@ def summ_search(mol, name,args, coord_Map = None,alg_Map=None,mol_template=None)
 				ps.ignoreSmoothingFailures=True
 				cids = rdDistGeom.EmbedMultipleConfs(mol, args.sample, params=ps)
 			else:
-				cids = rdDistGeom.EmbedMultipleConfs(mol, args.sample,ignoreSmoothingFailures=True randomSeed=args.seed)
+				cids = rdDistGeom.EmbedMultipleConfs(mol, args.sample,ignoreSmoothingFailures=True, randomSeed=args.seed)
 			if len(cids) == 0:
 				print("o  conformers initially sampled with random coordinates")
 				cids = rdDistGeom.EmbedMultipleConfs(mol, args.sample, randomSeed=args.seed, useRandomCoords=True, boxSizeMult=10.0,ignoreSmoothingFailures=True, numZeroFail=1000)
