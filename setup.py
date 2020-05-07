5b6964f0c721e2756e47583dff4ac84fabb68727
Juanvi Alegre-Requena <37852415+jvalegre@users.noreply.github.com>
2020-05-07 16:38:49 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
   download_url = 'https://github.com/jvalegre/DBGEN/archive/v_1.0.tar.gz',    
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Cheminformaticians',      # Define that your audience are developers
+    'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
