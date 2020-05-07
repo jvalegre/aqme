f3caad37c53e32894bf41634346517c1d96bb047
Juanvi Alegre-Requena <37852415+jvalegre@users.noreply.github.com>
2020-05-07 16:39:29 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'License :: OSI Approved :: MIT License',   
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
