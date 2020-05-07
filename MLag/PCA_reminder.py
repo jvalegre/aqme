a9387a5b0fe37c2e9aaf2983b9ddd4e50e3f12c4
jvalegre <juanvi89@hotmail.com>
2020-05-07 16:13:55 -0600

    Folder rearrangement

--- MLag/PCA_reminder.py
+++
@@ -1,6 +0,0 @@
-'''
-add an option that if you specify some parameters you
-can make a PCA plot with its corresponding scree plot, and then
-the program recognizes the elbow and takes that
-number of PCs to feed the model
-'''
