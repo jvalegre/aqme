4e9f638d56e196b604855b7ffce6d77c555d82a4
jvalegre <juanvi89@hotmail.com>
2020-05-15 11:18:53 -0600

    T

--- tests/test_pyCONFORT.py
+++ tests/test_pyCONFORT.py
@@ -5,6 +5,7 @@ import os
 import pytest
 import pandas as pd
 import subprocess
+import DBGEN
 
 # saves the working directory
 path = os.getcwd()
@@ -29,8 +30,8 @@ def read_energies(file): # parses the energies from sdf files - then used to fil
     ('pentane.smi', 'params_test5.yaml', 20, 13, 0, [-5.27175,-5.27175,-5.27175,-4.44183,-4.44183,-4.44183,-3.84858],0), # test rms_threshold = 1E-15
     ('pentane.smi', 'params_test6.yaml', 20, 3, 11, [-5.27175,-4.44183,-4.44183,-4.44183,-4.44183,-3.84858],0),
     ('pentane.smi', 'params_test7.yaml', 60, 56, 0, [-5.27175,-4.44183,-3.84858,-1.57172],0), # test sample = 'auto', auto_sample = 5
-    ('pentane.smi', 'params_test8.yaml', -1, -1, -1, -1, -1), # test num_rot_bonds = 1
-    ('pentane.smi', 'params_test9.yaml', -1, -1, -1, -1, -1), # test max_MolWt = 1
+    ('pentane.smi', 'params_test8.yaml', 'nan', 'nan', 'nan', 'nan', 'nan'), # test num_rot_bonds = 1
+    ('pentane.smi', 'params_test9.yaml', 'nan', 'nan', 'nan', 'nan', 'nan'), # test max_MolWt = 1
     ('pentane.smi', 'params_test10.yaml', 20, 17, 0, [2.52059,3.68961,4.94318],0), # test ff = 'UFF'
     ('pentane.smi', 'params_test11.yaml', 20, 0, 14, [-5.27037,-4.82921,-4.43450,-4.42888,-3.78645,-3.48832],0), # test opt_steps_RDKit = 40
     ('pentane.smi', 'params_test12.yaml', 20, 16, 0, [-5.27175,-4.44183,-3.84858,-1.57172],0), # test seed = 10
@@ -45,17 +46,13 @@ def test_confgen(smiles, params_file, n_confs, prefilter_confs_rdkit, filter_con
 
 	# Retrieving the generated CSV file
 	df_output = pd.read_csv(smiles.split('.')[0]+'-Duplicates Data.csv')
+
 	file = params_file.split('.')[0]
 
 	# tests for RDKit
-	try:
-		test_init_rdkit_confs = df_output['RDKIT-Initial-samples']
-		test_prefilter_rdkit_confs = df_output['RDKit-energy-duplicates']
-		test_filter_rdkit_confs = df_output['RDKit-RMSD-and-energy-duplicates']
-	except:
-		test_init_rdkit_confs = -1
-		test_prefilter_rdkit_confs = -1
-		test_filter_rdkit_confs = -1
+	test_init_rdkit_confs = df_output['RDKIT-Initial-samples']
+	test_prefilter_rdkit_confs = df_output['RDKit-energy-duplicates']
+	test_filter_rdkit_confs = df_output['RDKit-RMSD-and-energy-duplicates']
 
 	assert n_confs == test_init_rdkit_confs[0]
 	assert prefilter_confs_rdkit == test_prefilter_rdkit_confs[0]
@@ -63,10 +60,7 @@ def test_confgen(smiles, params_file, n_confs, prefilter_confs_rdkit, filter_con
 
 	# read the energies of the conformers
 	os.chdir(path+'/'+smiles.split('.')[0]+'/RDKit_generated_SDF_files')
-	try:
-		test_rdkit_E_confs = read_energies(smiles.split('.')[0]+'_rdkit.sdf')
-	except:
-		test_rdkit_E_confs = -1
+	test_rdkit_E_confs = read_energies(smiles.split('.')[0]+'_rdkit.sdf')
 
 	# test for energies
 	round_confs = [round(num, 3) for num in E_confs]
@@ -76,10 +70,8 @@ def test_confgen(smiles, params_file, n_confs, prefilter_confs_rdkit, filter_con
 	assert round_confs == test_round_confs
 
 	# tests charge
-	try:
-		test_charge = df_output['Overall charge']
-	except:
-		test_charge = -1
+	test_charge = df_output['Overall charge']
+
 	assert charge == test_charge[0]
 
 # MISSING CHECKS:
