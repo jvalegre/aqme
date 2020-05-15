72c5775a3cf8ea554092a3566b4f3c304306d7f0
jvalegre <juanvi89@hotmail.com>
2020-05-15 14:54:59 -0600

    Travis

--- tests/test_pyCONFORT.py
+++ tests/test_pyCONFORT.py
@@ -33,25 +33,27 @@ def read_energies(file): # parses the energies from sdf files - then used to fil
 	return energies
 
 # tests for the DBGEN module with organic molecules and metal complexes
-@pytest.mark.parametrize("smiles, params_file, n_confs, prefilter_confs_rdkit, filter_confs_rdkit, E_confs, charge",
+@pytest.mark.parametrize("smiles, params_file, n_confs, prefilter_confs_rdkit, filter_confs_rdkit, E_confs, charge, xTB_ANI1",
 [
-    ('pentane.smi', 'params_test1.yaml', 240, 236, 0, [-5.27175,-4.44184,-3.84858,-1.57172],0), # test sample = 'auto', auto_sample = 20
-    ('pentane.smi', 'params_test2.yaml', 20, 16, 0, [-5.27175, -4.44184, -3.84858, -1.57172],0), # test sample = 20
-    ('pentane.smi', 'params_test3.yaml', 20, 2, 13, [-5.27175, -4.44184, -4.44184, -3.84858, -1.57172],0), # test initial_energy_threshold = 1E-10
-    ('pentane.smi', 'params_test4.yaml', 20, 10, 0, [-5.27175, -5.27175, -5.27175, -5.27175, -4.44184, -4.44184, -4.44184, -4.44184, -3.84858, -1.57172],0), # test energy_threshold = 1E-15
-    ('pentane.smi', 'params_test5.yaml', 20, 10, 0, [-5.27175, -5.27175, -5.27175, -5.27175, -4.44184, -4.44184, -4.44184, -4.44184, -3.84858, -1.57172],0), # test rms_threshold = 1E-15
-    ('pentane.smi', 'params_test6.yaml', 20, 2, 11, [-5.27175, -4.44184, -4.44184, -4.44184, -4.44184, -3.84858, -1.57172],0),
-    ('pentane.smi', 'params_test7.yaml', 60, 56, 0, [-5.27175, -4.44184, -3.84858, -1.57172],0), # test sample = 'auto', auto_sample = 5
-    ('pentane.smi', 'params_test8.yaml', 'nan', 'nan', 'nan', 'nan', 'nan'), # test num_rot_bonds = 1
-    ('pentane.smi', 'params_test9.yaml', 'nan', 'nan', 'nan', 'nan', 'nan'), # test max_MolWt = 1
-    ('pentane.smi', 'params_test10.yaml', 20, 16, 0, [2.52059, 3.68961, 4.94318, 6.51778],0), # test ff = 'UFF'
-    ('pentane.smi', 'params_test11.yaml', 20, 0, 8, [-5.26093, -4.41687, -4.39313, -4.10961, -3.93585, -2.95568, -2.43353, -2.03709, -1.51856, -1.45757, -0.22202, 0.46406],0), # test opt_steps_RDKit = 40
-    ('Ir_hexacoord.smi', 'params_Ir_test1.yaml', 1440, 1434, 0, [-5.26093, -4.41687, -4.39313, -4.10961, -3.93585, -2.95568, -2.43353, -2.03709, -1.51856, -1.45757, -0.22202, 0.46406],1), # test opt_steps_RDKit = 40
-    ('Ir_Ag_complex.smi', 'params_Ag_Au_test1.yaml', 2, 0, 0, [-5.26093, -4.41687, -4.39313, -4.10961, -3.93585, -2.95568, -2.43353, -2.03709, -1.51856, -1.45757, -0.22202, 0.46406],1), # test opt_steps_RDKit = 40
-    ('Pd_squareplanar.smi', 'params_Pd_test1.yaml', 120, 111, 0, [-5.26093, -4.41687, -4.39313, -4.10961, -3.93585, -2.95568, -2.43353, -2.03709, -1.51856, -1.45757, -0.22202, 0.46406],1), # test opt_steps_RDKit = 40
+    ('pentane.smi', 'params_test1.yaml', 240, 236, 0, [-5.27175,-4.44184,-3.84858,-1.57172],0, False), # test sample = 'auto', auto_sample = 20
+    ('pentane.smi', 'params_test2.yaml', 20, 16, 0, [-5.27175, -4.44184, -3.84858, -1.57172],0, False), # test sample = 20
+    ('pentane.smi', 'params_test3.yaml', 20, 2, 13, [-5.27175, -4.44184, -4.44184, -3.84858, -1.57172],0, False), # test initial_energy_threshold = 1E-10
+    ('pentane.smi', 'params_test4.yaml', 20, 10, 0, [-5.27175, -5.27175, -5.27175, -5.27175, -4.44184, -4.44184, -4.44184, -4.44184, -3.84858, -1.57172],0, False), # test energy_threshold = 1E-15
+    ('pentane.smi', 'params_test5.yaml', 20, 10, 0, [-5.27175, -5.27175, -5.27175, -5.27175, -4.44184, -4.44184, -4.44184, -4.44184, -3.84858, -1.57172],0, False), # test rms_threshold = 1E-15
+    ('pentane.smi', 'params_test6.yaml', 20, 2, 11, [-5.27175, -4.44184, -4.44184, -4.44184, -4.44184, -3.84858, -1.57172],0, False),
+    ('pentane.smi', 'params_test7.yaml', 60, 56, 0, [-5.27175, -4.44184, -3.84858, -1.57172],0, False), # test sample = 'auto', auto_sample = 5
+    ('pentane.smi', 'params_test8.yaml', 'nan', 'nan', 'nan', 'nan', 'nan', False), # test num_rot_bonds = 1
+    ('pentane.smi', 'params_test9.yaml', 'nan', 'nan', 'nan', 'nan', 'nan', False), # test max_MolWt = 1
+    ('pentane.smi', 'params_test10.yaml', 20, 16, 0, [2.52059, 3.68961, 4.94318, 6.51778],0, False), # test ff = 'UFF'
+    ('pentane.smi', 'params_test11.yaml', 20, 0, 8, [-5.26093, -4.41687, -4.39313, -4.10961, -3.93585, -2.95568, -2.43353, -2.03709, -1.51856, -1.45757, -0.22202, 0.46406],0, False), # test opt_steps_RDKit = 40
+    ('pentane.smi', 'params_test12.yaml', 20, 16, 0, [-5.27175,-4.44184,-3.84858,-1.57172],0, True), # test xTB = True
+    ('pentane.smi', 'params_test13.yaml', 20, 16, 0, [-5.27175,-4.44184,-3.84858,-1.57172],0, True), # test ANI1ccx = True
+    ('Ir_hexacoord.smi', 'params_Ir_test1.yaml', 1440, 1434, 0, [-5.26093, -4.41687, -4.39313, -4.10961, -3.93585, -2.95568, -2.43353, -2.03709, -1.51856, -1.45757, -0.22202, 0.46406],1, False), # test 1 single metal
+    ('Ir_Ag_complex.smi', 'params_Ag_Au_test1.yaml', 2, 0, 0, [-5.26093, -4.41687, -4.39313, -4.10961, -3.93585, -2.95568, -2.43353, -2.03709, -1.51856, -1.45757, -0.22202, 0.46406],1, False), # test 2 metals
+    ('Pd_squareplanar.smi', 'params_Pd_test1.yaml', 120, 111, 0, [-5.26093, -4.41687, -4.39313, -4.10961, -3.93585, -2.95568, -2.43353, -2.03709, -1.51856, -1.45757, -0.22202, 0.46406],1, False), # test squareplanar template
 ])
 
-def test_confgen(smiles, params_file, n_confs, prefilter_confs_rdkit, filter_confs_rdkit, E_confs, charge):
+def test_confgen(smiles, params_file, n_confs, prefilter_confs_rdkit, filter_confs_rdkit, E_confs, charge, xTB_ANI1):
 	# gets into the directory for testing SMILES
 	os.chdir(path+'/'+smiles.split('.')[0])
 
@@ -75,7 +77,7 @@ def test_confgen(smiles, params_file, n_confs, prefilter_confs_rdkit, filter_con
 	# read the energies of the conformers
 	os.chdir(path+'/'+smiles.split('.')[0]+'/RDKit_generated_SDF_files')
 	test_rdkit_E_confs = read_energies(smiles.split('.')[0]+'_rdkit.sdf')
-	
+
 	# test for energies
 	try:
 		test_round_confs = [round(num, precision) for num in test_rdkit_E_confs]
