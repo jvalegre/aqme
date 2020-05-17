97aeee6b4700c262b993a121c9de335aa7d3bf80
jvalegre <juanvi89@hotmail.com>
2020-05-17 16:46:03 -0600

    t

--- tests/test_pyCONFORT.py
+++ tests/test_pyCONFORT.py
@@ -123,7 +123,7 @@ def test_confgen(params_file):
 @pytest.mark.parametrize("com_file, params_file, gen_ecp",
 [
     ('Pd_squareplanar_conformer_1.com','params_comb_test1.yaml', False), # test gen
-    ('Pd_squareplanar2_conformer_1.com','params_comb_test2.yaml', True'), # test gen_ecp
+    ('Pd_squareplanar2_conformer_1.com','params_comb_test2.yaml', True), # test gen_ecp
 ])
 
 def test_confgen(com_file, params_file):
