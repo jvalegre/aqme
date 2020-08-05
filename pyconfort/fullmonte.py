97559f4f33b2fd024162f7d5bb69ec786c54a5b0
shreesowndarya <sowndary64@gmail.com>
2020-08-05 12:02:46 -0600

    fixing dihedral

--- pyconfort/fullmonte.py
+++ pyconfort/fullmonte.py
@@ -10,6 +10,7 @@ import sys
 import subprocess
 import time
 import numpy as np
+import math
 import random
 from rdkit.Chem import AllChem as Chem
 from rdkit.Chem import rdMolTransforms, PropertyMol, rdDistGeom, rdMolAlign, Lipinski
@@ -58,8 +59,11 @@ def minimize_rdkit_energy(mol,conf,args,log):
 	return GetFF
 
 def rotate_dihedral(mol_rot,dih_rot,args,conf):
+	rad_range = np.arange(0.0, 360.0,args.ang_fullmonte)
 	for i,_ in enumerate(dih_rot):
-		rdMolTransforms.SetDihedralRad(mol_rot.GetConformer(conf),*dih_rot[i],value=args.ang_fullmonte)
+		rad_ang = random.choice(rad_range)
+		rad = math.pi*rad_ang/180.0
+		rdMolTransforms.SetDihedralRad(mol_rot.GetConformer(conf),*dih_rot[i],value=rad)
 	return mol_rot
 
 
@@ -84,24 +88,22 @@ def generating_conformations_fullmonte(name,args,rotmatches,log,selectedcids_rdk
 	# array for each each unique from rdkit
 
 	bar = IncrementalBar('o  Generating conformations based on dihedral rotation', max = len(fmmols))
-	# no=1
 	for mol_fm in fmmols:
 
 		unique_mol,c_energy = [],[]
 
-		#STEP 1: Use start conformation for and append to unique list #DO this uniformly by using a uniform sampling tecnique
+		#STEP 1: Use start conformation for and append to unique list
 		nsteps = 1
 		unique_mol.append(mol_fm)
 		c_energy.append(float(mol_fm.GetProp("Energy")))
 
-
 		while nsteps < args.nsteps_fullmonte:
 			#STEP 2: Choose mol object form unique_mol:
-			mol_rot = random.sample(unique_mol,1)[0]
+			mol_rot = random.choices(unique_mol,k=1)[0]
 
 			#STEP 3: Choose randmon subset of dihedral from rotmatches
 			if len(rotmatches) > args.nrot_fullmonte:
-				dih_rot = random.sample(rotmatches, k=args.nrot_fullmonte) #k can be varied base on user definitions
+				dih_rot = random.choices(rotmatches, k=args.nrot_fullmonte) #k can be varied base on user definitions
 			else:
 				dih_rot = random.choices(rotmatches, k=len(rotmatches)) #k can be varied base on user definitions
 			#STEP 4: for the given conformation, then apply a random rotation to each torsion in the subset
@@ -124,22 +126,18 @@ def generating_conformations_fullmonte(name,args,rotmatches,log,selectedcids_rdk
 						break
 
 			if not exclude_conf:
+				rot_mol.SetProp('_Name',mol_fm.GetProp("_Name")+'_'+str(nsteps))
 				unique_mol.append(rot_mol)
 				c_energy.append(energy)
 
 			#STEP 6: ANALYSE THE UNIQUE list for lowest energy, reorder the uniques if greater the given thershold remove
 			globmin = min(c_energy)
+			#print(energy,globmin)
 			for ind,_ in enumerate(c_energy):
 				if  globmin - c_energy[ind] > args.ewin_fullmonte:
 					c_energy.pop(ind)
 					unique_mol.pop(ind)
 			nsteps += 1
-
-		# sdtemp = Chem.SDWriter(str(no)+'_'+'temp'+args.output)
-		# for mol in unique_mol:
-		# 	sdtemp.write(mol)
-		# sdtemp.close()
-		# no+=1
 
 		#appending to all_uniq Mol
 		all_unique_mol.append(unique_mol)
@@ -171,17 +169,15 @@ def generating_conformations_fullmonte(name,args,rotmatches,log,selectedcids_rdk
 		if coord_Map is None and alg_Map is None and mol_template is None:
 			if args.metal_complex:
 				set_metal_atomic_number(mol_ro,args)
-			mol_ro.SetProp('_Name',mol_rot.GetProp("_Name")+'_'+str(i))
 			sdwriter.write(mol_ro)
 		else:
 			mol_ro_realigned,_ = realign_mol(mol_ro,-1,coord_Map, alg_Map, mol_template,args,log)
 			if args.metal_complex:
 				set_metal_atomic_number(mol_ro_realigned,args)
-			mol_ro.SetProp('_Name',mol_rot.GetProp("_Name")+'_'+str(i+1))
 			sdwriter.write(mol_ro_realigned)
 
 	status = 1
 	#removes the rdkit file
-	os.remove(name+'_'+'rdkit'+args.output)
+	#os.remove(name+'_'+'rdkit'+args.output)
 
 	return status
