ccfed9c8e73ea03bce19a983cff9534804c965d5
shreesowndarya <sowndary64@gmail.com>
2020-08-20 14:35:44 -0600

    final workign csearch! hopefully!

--- pyconfort/csearch.py
+++ pyconfort/csearch.py
@@ -308,10 +308,7 @@ def genConformer_r(mol, conf, i, matches, degree, sdwriter,args,name,log):
 		while deg < 360.0:
 			rad = math.pi*deg / 180.0
 			rdMolTransforms.SetDihedralRad(mol.GetConformer(conf),*matches[i],value=rad)
-			#recalculating energies after rotation
-			# GetFF = minimize_rdkit_energy(mol,conf,args,log)
-			# mol.SetProp("Energy",GetFF.CalcEnergy())
-			# mol.SetProp('_Name',name)
+			mol.SetProp('_Name',name)
 			total += genConformer_r(mol, conf, i+1, matches, degree, sdwriter,args,name,log)
 			deg += degree
 		return total
@@ -365,7 +362,8 @@ def min_after_embed(mol,cids,name,initial_confs,rotmatches,dup_data,dup_data_idx
 
 	for i, cid in enumerate(cids):
 		outmols[cid].SetProp('_Name', name +' '+ str(i+1))
-		outmols[cid].SetProp('Energy', cenergy[cid])
+		outmols[cid].SetProp('Energy', str(cenergy[cid]))
+
 
 	# sorts the energies
 	cids = list(range(len(outmols)))
@@ -400,6 +398,10 @@ def min_after_embed(mol,cids,name,initial_confs,rotmatches,dup_data,dup_data_idx
 
 		total = 0
 		for conf in selectedcids_rdkit:
+			if args.CSEARCH=='summ':
+				sdwriter.write(outmols[conf],conf)
+				for m in rotmatches:
+					rdMolTransforms.SetDihedralDeg(outmols[conf].GetConformer(conf),*m,180.0)
 			total += genConformer_r(outmols[conf], conf, 0, rotmatches, args.degree, sdwriter ,args,outmols[conf].GetProp('_Name'),log)
 			bar.next()
 		bar.finish()
@@ -475,22 +477,13 @@ def dihedral_filter_and_sdf(name,args,log,dup_data,dup_data_idx,coord_Map, alg_M
 		log.write("Could not open "+ name+args.output)
 		sys.exit(-1)
 
-
 	for i, rd_mol_i in enumerate(rdmols):
-		# print(rd_mol_i.GetProp('Energy'))
-		# GetFF = minimize_rdkit_energy(rd_mol_i,-1,args,log)
-		# rotated_energy.append(GetFF.CalcEnergy())
-
-		# rotated_energy.append(float(rd_mol_i.GetProp('Energy')))
-
 		if coord_Map is None and alg_Map is None and mol_template is None:
 			GetFF = minimize_rdkit_energy(rd_mol_i,-1,args,log)
-			rotated_energy.append(GetFF.CalcEnergy())
-
-		# id template realign before doing calculations
+			rotated_energy.append(float(GetFF.CalcEnergy()))
 		else:
 			rd_mol_i,GetFF = realign_mol(rd_mol_i,-1,coord_Map, alg_Map, mol_template,args,log)
-			rotated_energy.append(GetFF.CalcEnergy())
+			rotated_energy.append(float(GetFF.CalcEnergy()))
 
 	rotated_cids = list(range(len(rdmols)))
 	sorted_rotated_cids = sorted(rotated_cids, key = lambda cid: rotated_energy[cid])
@@ -506,15 +499,10 @@ def dihedral_filter_and_sdf(name,args,log,dup_data,dup_data_idx,coord_Map, alg_M
 	for i, cid in enumerate(selectedcids_rotated):
 		mol_rd = Chem.RWMol(rdmols[cid])
 		mol_rd.SetProp('_Name',rdmols[cid].GetProp('_Name')+' '+str(i))
-		# if coord_Map is None and alg_Map is None and mol_template is None:
+		mol_rd.SetProp('Energy',str(rotated_energy[cid]))
 		if args.metal_complex:
 			set_metal_atomic_number(mol_rd,args)
 		sdwriter_rd.write(mol_rd)
-		# else:
-		# 	# mol_rd_realigned,_ = realign_mol(mol_rd,-1,coord_Map, alg_Map, mol_template,args,log)
-		# 	if args.metal_complex:
-		# 		set_metal_atomic_number(mol_rd_realigned,args)
-		# 	sdwriter_rd.write(mol_rd_realigned)
 
 	sdwriter_rd.close()
 	status = 1
@@ -532,7 +520,7 @@ def summ_search(mol, name,args,log,dup_data,dup_data_idx, coord_Map = None, alg_
 		if args.CSEARCH=='summ' and len(rotmatches) != 0:
 			status = dihedral_filter_and_sdf(name,args,log,dup_data,dup_data_idx,coord_Map, alg_Map, mol_template)
 
-			#removes the rdkit file
-			os.remove(name+'_'+'rdkit'+args.output)
+			# removes the rdkit file
+			# os.remove(name+'_'+'rdkit'+args.output)
 
 	return status
