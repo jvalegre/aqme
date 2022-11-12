e40e031d8f3fb2256bfda0faf1c36797ad9483df
Juanvi Alegre-Requena <juanvi89@hotmail.com>
2022-11-12 09:39:15 +0100

    testing CMIN

--- tests/test_cmin.py
+++ tests/test_cmin.py
@@ -67,124 +67,124 @@ def test_cmin_methods(
     os.chdir(w_dir_main)
 
 
-# tests for parameters of cmin paramters
-@pytest.mark.parametrize(
-    "program, sdf, metal_complex,metal_atoms,metal_oxi,complex_type, charge, mult, xtb_solvent, ewin_cmin, initial_energy_threshold, energy_threshold,rms_threshold,xtb_accuracy,xtb_electronic_temperature, xtb_max_iterations, output_nummols",
-    [
-        # tests for conformer generation with RDKit
-        (
-            "xtb",
-            "pentane_rdkit.sdf",
-            False,
-            None,
-            None,
-            None,
-            0,
-            1,
-            "Chloroform",
-            5,
-            0.003,
-            0.4,
-            0.5,
-            0.01,
-            298,
-            500,
-            4,
-        ),
-        (
-            "xtb",
-            "Pd_complex_0_rdkit.sdf",
-            True,
-            ["Pd"],
-            [2],
-            "squareplanar",
-            0,
-            1,
-            "none",
-            8,
-            0.004,
-            0.3,
-            0.2,
-            0.02,
-            300,
-            400,
-            2,
-        ),
-    ],
-)
-def test_cmin_xtb_parameters(
-    program,
-    sdf,
-    metal_complex,
-    metal_atoms,
-    metal_oxi,
-    complex_type,
-    charge,
-    mult,
-    xtb_solvent,
-    ewin_cmin,
-    initial_energy_threshold,
-    energy_threshold,
-    rms_threshold,
-    xtb_accuracy,
-    xtb_electronic_temperature,
-    xtb_max_iterations,
-    output_nummols,
-):
-    os.chdir(cmin_xtb_dir)
-    # runs the program with the different tests
-    if not metal_complex:
-        cmin(
-            w_dir_main=cmin_xtb_dir,
-            program=program,
-            files=sdf,
-            charge=charge,
-            mult=mult,
-            xtb_solvent=xtb_solvent,
-            ewin_cmin=ewin_cmin,
-            initial_energy_threshold=initial_energy_threshold,
-            energy_threshold=energy_threshold,
-            rms_threshold=rms_threshold,
-            xtb_accuracy=xtb_accuracy,
-            xtb_electronic_temperature=xtb_electronic_temperature,
-            xtb_max_iterations=xtb_max_iterations,
-        )
-    else:
-        cmin(
-            w_dir_main=cmin_xtb_dir,
-            program=program,
-            files=sdf,
-            metal_complex=metal_complex,
-            metal_atoms=metal_atoms,
-            metal_oxi=metal_oxi,
-            complex_type=complex_type,
-            charge=charge,
-            mult=mult,
-            xtb_solvent=xtb_solvent,
-            ewin_cmin=ewin_cmin,
-            initial_energy_threshold=initial_energy_threshold,
-            energy_threshold=energy_threshold,
-            rms_threshold=rms_threshold,
-            xtb_accuracy=xtb_accuracy,
-            xtb_electronic_temperature=xtb_electronic_temperature,
-            xtb_max_iterations=xtb_max_iterations,
-        )
+# # tests for parameters of cmin paramters
+# @pytest.mark.parametrize(
+#     "program, sdf, metal_complex,metal_atoms,metal_oxi,complex_type, charge, mult, xtb_solvent, ewin_cmin, initial_energy_threshold, energy_threshold,rms_threshold,xtb_accuracy,xtb_electronic_temperature, xtb_max_iterations, output_nummols",
+#     [
+#         # tests for conformer generation with RDKit
+#         (
+#             "xtb",
+#             "pentane_rdkit.sdf",
+#             False,
+#             None,
+#             None,
+#             None,
+#             0,
+#             1,
+#             "Chloroform",
+#             5,
+#             0.003,
+#             0.4,
+#             0.5,
+#             0.01,
+#             298,
+#             500,
+#             4,
+#         ),
+#         (
+#             "xtb",
+#             "Pd_complex_0_rdkit.sdf",
+#             True,
+#             ["Pd"],
+#             [2],
+#             "squareplanar",
+#             0,
+#             1,
+#             "none",
+#             8,
+#             0.004,
+#             0.3,
+#             0.2,
+#             0.02,
+#             300,
+#             400,
+#             2,
+#         ),
+#     ],
+# )
+# def test_cmin_xtb_parameters(
+#     program,
+#     sdf,
+#     metal_complex,
+#     metal_atoms,
+#     metal_oxi,
+#     complex_type,
+#     charge,
+#     mult,
+#     xtb_solvent,
+#     ewin_cmin,
+#     initial_energy_threshold,
+#     energy_threshold,
+#     rms_threshold,
+#     xtb_accuracy,
+#     xtb_electronic_temperature,
+#     xtb_max_iterations,
+#     output_nummols,
+# ):
+#     os.chdir(cmin_xtb_dir)
+#     # runs the program with the different tests
+#     if not metal_complex:
+#         cmin(
+#             w_dir_main=cmin_xtb_dir,
+#             program=program,
+#             files=sdf,
+#             charge=charge,
+#             mult=mult,
+#             xtb_solvent=xtb_solvent,
+#             ewin_cmin=ewin_cmin,
+#             initial_energy_threshold=initial_energy_threshold,
+#             energy_threshold=energy_threshold,
+#             rms_threshold=rms_threshold,
+#             xtb_accuracy=xtb_accuracy,
+#             xtb_electronic_temperature=xtb_electronic_temperature,
+#             xtb_max_iterations=xtb_max_iterations,
+#         )
+#     else:
+#         cmin(
+#             w_dir_main=cmin_xtb_dir,
+#             program=program,
+#             files=sdf,
+#             metal_complex=metal_complex,
+#             metal_atoms=metal_atoms,
+#             metal_oxi=metal_oxi,
+#             complex_type=complex_type,
+#             charge=charge,
+#             mult=mult,
+#             xtb_solvent=xtb_solvent,
+#             ewin_cmin=ewin_cmin,
+#             initial_energy_threshold=initial_energy_threshold,
+#             energy_threshold=energy_threshold,
+#             rms_threshold=rms_threshold,
+#             xtb_accuracy=xtb_accuracy,
+#             xtb_electronic_temperature=xtb_electronic_temperature,
+#             xtb_max_iterations=xtb_max_iterations,
+#         )
 
-    # tests here
-    file = str("CMIN/" + program + "/" + sdf.split(".")[0] + "_" + program + ".sdf")
-    file2 = str(
-        "CMIN/" + program + "/" + sdf.split(".")[0] + "_" + program + "_all_confs.sdf"
-    )
-    assert os.path.exists(file)
-    assert os.path.exists(file2)
+#     # tests here
+#     file = str("CMIN/" + program + "/" + sdf.split(".")[0] + "_" + program + ".sdf")
+#     file2 = str(
+#         "CMIN/" + program + "/" + sdf.split(".")[0] + "_" + program + "_all_confs.sdf"
+#     )
+#     assert os.path.exists(file)
+#     assert os.path.exists(file2)
 
-    mols = rdkit.Chem.SDMolSupplier(file, removeHs=False)
-    assert len(mols) == output_nummols
+#     mols = rdkit.Chem.SDMolSupplier(file, removeHs=False)
+#     assert len(mols) == output_nummols
 
-    assert int(mols[0].GetProp("Real charge")) == charge
-    assert int(mols[0].GetProp("Mult")) == mult
+#     assert int(mols[0].GetProp("Real charge")) == charge
+#     assert int(mols[0].GetProp("Mult")) == mult
 
-    os.chdir(w_dir_main)
+#     os.chdir(w_dir_main)
 
 
 # tests for removing foler
@@ -201,7 +201,6 @@ def test_cmin_xtb_parameters(
     ],
 )
 def test_remove(remove, folder, file):
-    # os.chdir(w_dir_main)
     shutil.rmtree(w_dir_main + "/" + folder)
     for f in glob.glob(file):
         os.remove(f)
