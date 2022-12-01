55ed69dc531423638da7bde41da8b54dc94c8568
Raúl Pérez-Soto <rperezsoto.research@gmail.com>
2022-12-01 12:32:26 -0700

    Added the basic requirements for the automatically building the docs with readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,9 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
+# Ensure that modules can be imported without installing aqme
+sys.path.insert(0, os.path.abspath('..')) 
+
 
 
 # -- Project information -----------------------------------------------------
