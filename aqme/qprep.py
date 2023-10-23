47289e6d686ab6971a66b0ce1f8d01cdc9b838d2
shreesowndarya <sowndarya64@gmail.com>
2023-10-23 11:13:12 -0600

    qprep_charge_mult

--- aqme/qprep.py
+++ aqme/qprep.py
@@ -186,6 +186,7 @@ class qprep:
                     sdf_files.append(f"{file.split('.pdb')[0]}.sdf")
 
                 else:
+
                     sdf_files.append(file)
 
                 for sdf_file in sdf_files:
@@ -445,11 +446,15 @@ class qprep:
                 try:
                     charge = int(mol.GetProp("Real charge"))
                 except KeyError:
-                    pass
+                    charge = Chem.GetFormalCharge(mol)
                 try:
                     mult = int(mol.GetProp("Mult"))
                 except KeyError:
-                    pass
+                    NumRadicalElectrons = 0
+                    for Atom in mol.GetAtoms():
+                        NumRadicalElectrons += Atom.GetNumRadicalElectrons()
+                    TotalElectronicSpin = NumRadicalElectrons / 2
+                    mult = int((2 * TotalElectronicSpin) + 1)
 
             elif file_format in ["log", "out"]:
                 # detect QM program and number of atoms
