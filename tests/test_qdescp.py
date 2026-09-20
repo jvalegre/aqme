02d7782b2825ac98210017f56e62c721df7038c2
cruzolijavier <javier.cruz.oliver@gmail.com>
2026-09-20 12:43:07 +0200

    Change, deleted nprocs 4 in tests, even if CI only has 4 vCPUs

--- tests/test_qdescp.py
+++ tests/test_qdescp.py
@@ -204,7 +204,6 @@ def test_qdescp_xtb(file):
     qdescp_kwargs = {
         "input": f'{qdescp_input_dir.joinpath(file_qdescp)}',
         "destination": f'{folder_qdescp}',
-        "nprocs": 4,
     }
 
     if file == 'test_atom.csv':
@@ -243,8 +242,6 @@ def test_qdescp_xtb(file):
             f'{qdescp_input_dir.joinpath(file_qdescp)}',
             "--destination",
             f'{folder_qdescp}',
-            "--nprocs",
-            "4",
             "--qdescp_atoms",
             '[1]',
         ]
@@ -556,7 +553,6 @@ def test_qdescp_missing(
     qdescp(
         files=f'{qdescp_empty_dir}/*.sdf',
         destination=f'{folder_qdescp}',
-        nprocs=4,
     )
 
     # checking csv file
@@ -610,7 +606,6 @@ def test_qdescp_sdf(
             qdescp_atoms=['As'],
             charge=-1,
             mult=1,
-            nprocs=4,
         )
         name_1 = 'conf_72'
         name_2 = 'conf_73'
@@ -622,7 +617,6 @@ def test_qdescp_sdf(
         qdescp(
             files=f'{qdescp_sdf_dir}/*.sdf',
             destination=f'{folder_qdescp}',
-            nprocs=4,
         )
         name_1 = 'mol1'
         name_2 = 'mol_2'
@@ -658,7 +652,6 @@ def test_qdescp_xyz_auto_charge_mult(tmp_path, monkeypatch):
     qdescp(
         files=xyz_files,
         destination=str(tmp_path / 'QDESCP'),
-        nprocs=4,
     )
 
     for name in xyz_structures:
@@ -699,7 +692,6 @@ def test_qdescp_csv(
     qdescp(
         input=f'{qdescp_csv_dir}/{file}',
         destination=f'{folder_qdescp}',
-        nprocs=4,
     )
 
     # checking csv file
@@ -761,7 +753,6 @@ def test_au_csv(
     qdescp_kwargs = {
         "input": f'{qdescp_au_dir}/{file}',
         "destination": f'{folder_qdescp}',
-        "nprocs": 4,
     }
     
     if run_test == 2:
@@ -872,7 +863,6 @@ def test_qdescp_nmr(json_files):
         files=json_files,
         destination=qdescp_input_dir,
         program="nmr",
-        nprocs=4,
         nmr_slope=nmr_slope,
         nmr_intercept=nmr_intercept,
         nmr_experim=f'{qdescp_input_dir}/Experimental_NMR_shifts.csv',
